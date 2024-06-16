73a5a56a51985949952f7429671dacba49290381
ivanbal93 <ivanbal93@gmail.com>
2024-06-16 17:46:12 +0400

    added weather vidget

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,8 @@
 <header id="header" class="header d-flex flex-column">
     <i class="header-toggle d-xl-none bi bi-list"></i>
     <hr>
+    <div id="id3ee3759029ae5" a='{"t":"v","v":"1.2","lang":"ru","locs":[967],"ssot":"c","sics":"ms","cbkg":"#00000000","cfnt":"#000000","cprb":"#FFFFFF00","cprf":"rgba(255,255,255,1)","slfs":"16","slbr":"8","ent":"bool","eev":"bool","edy":"bool","emr":"bool", "ephw":"bool"}'>Источник данных о погоде: <a href="https://meteolabs.org/погода_туапсе/завтра/">Туапсе погода по часам завтра</a></div><script async src="https://static1.meteolabs.org/widgetjs/?id=id3ee3759029ae5"></script>
+    <hr>
     <nav id="navmenu" class="navmenu">
         <ul>
             <li><a href="#" class="active"><i class="bi bi-house navicon"></i>Главная</a></li>
